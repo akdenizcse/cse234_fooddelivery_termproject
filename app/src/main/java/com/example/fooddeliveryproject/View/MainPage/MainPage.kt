1672ebd03d3b22d2f43cf74fc7d95c8f631aaf8a
İsmail Babacan <114854598+ismailbabacann@users.noreply.github.com>
2024-05-29 02:30:06 +0300

    restorant part changed to more practical version

--- app/src/main/java/com/example/fooddeliveryproject/View/MainPage/MainPage.kt
+++ app/src/main/java/com/example/fooddeliveryproject/View/MainPage/MainPage.kt
@@ -1,17 +1,25 @@
 package com.example.fooddeliveryproject.View.MainPage
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -21,31 +29,40 @@ import com.example.fooddeliveryproject.View.SpecialWidgets.CarouselCardOrigin
 import com.example.fooddeliveryproject.View.SpecialWidgets.CarouselItem
 import com.example.fooddeliveryproject.View.SpecialWidgets.CategoryCard
 import com.example.fooddeliveryproject.View.SpecialWidgets.Header
+import com.example.fooddeliveryproject.View.SpecialWidgets.Restaurant
 import com.example.fooddeliveryproject.View.SpecialWidgets.Restorants
 import com.example.fooddeliveryproject.View.SpecialWidgets.SearchBar
-
-
+import com.example.fooddeliveryproject.View.SpecialWidgets.TopRestaurantsSection
 
 @Composable
 fun MainPage() {
-    Column (Modifier.fillMaxSize()){
+    LazyColumn(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.spacedBy(8.dp) // Optional: space between items
+    ) {
+        item {
+            Spacer(modifier = Modifier.height(8.dp))
             Header()
+        }
+        item {
             SearchBar()
+        }
+        item {
             CarouselCardOrigin()
+        }
+        item {
             CategoryCard()
-            Restorants()
-    }
+        }
 
+        item {
+            Restorants()
+        }
+
+    }
 }
 
-
-
-
-
-
-    @Preview(showBackground = true)
-    @Composable
-    fun DefaultPreview3() {
-     MainPage()
-
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview3() {
+    MainPage()
 }
--- app/src/main/java/com/example/fooddeliveryproject/View/SpecialWidgets/Restorants.kt
+++ app/src/main/java/com/example/fooddeliveryproject/View/SpecialWidgets/Restorants.kt
@@ -1,15 +1,19 @@
 package com.example.fooddeliveryproject.View.SpecialWidgets
 
-
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -25,30 +29,29 @@ data class Restaurant(
 
 @Composable
 fun RestaurantItem(restaurant: Restaurant) {
-    Row(
+    Column(
         modifier = Modifier
-            .fillMaxWidth()
             .padding(8.dp),
-        verticalAlignment = Alignment.CenterVertically
+        //horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Image(
             painter = painterResource(id = restaurant.imageRes),
             contentDescription = restaurant.name,
             modifier = Modifier
-                .size(64.dp)
-                .padding(end = 8.dp)
+                .clip(shape = RoundedCornerShape(16.dp))
+                .size(width = 200.dp, height = 200.dp)
         )
         Column(
-            modifier = Modifier.weight(1f)
+            modifier = Modifier
+                .padding(6.dp)
         ) {
             Text(
                 text = restaurant.name,
-                style = MaterialTheme.typography.subtitle1,
-                color = Color.Black
+                style = MaterialTheme.typography.h6,
+                color = Color.DarkGray
             )
             Text(
                 text = restaurant.location,
-                style = MaterialTheme.typography.body2,
                 color = Color.Gray
             )
         }
@@ -63,7 +66,6 @@ fun RestaurantItem(restaurant: Restaurant) {
             )
             Text(
                 text = "${restaurant.rating} (${restaurant.reviewCount}+ yorum)",
-                style = MaterialTheme.typography.body2,
                 color = Color.Gray
             )
         }
@@ -73,7 +75,7 @@ fun RestaurantItem(restaurant: Restaurant) {
 @Composable
 fun TopRestaurantsSection(restaurants: List<Restaurant>) {
     Column(
-        modifier = Modifier.padding(16.dp)
+        modifier = Modifier.padding(6.dp)
     ) {
         Row(
             modifier = Modifier.fillMaxWidth(),
@@ -91,8 +93,8 @@ fun TopRestaurantsSection(restaurants: List<Restaurant>) {
             )
         }
 
-        LazyColumn(
-            modifier = Modifier.padding(top = 8.dp)
+        LazyRow(
+            modifier = Modifier.padding(8.dp)
         ) {
             items(restaurants) { restaurant ->
                 RestaurantItem(restaurant = restaurant)
@@ -104,22 +106,17 @@ fun TopRestaurantsSection(restaurants: List<Restaurant>) {
 @Composable
 fun Restorants() {
     val restaurants = listOf(
-        Restaurant(R.drawable.noodle, "Çorbacı Şükrü", "Kültür, Antalya", 4.1f, 500),
-        Restaurant(R.drawable.doner, "Dürümcü Bedir", "Konyaaltı, Antalya", 3.6f, 400),
-        Restaurant(R.drawable.hamburgerr, "Burger King", "Güvercin, Kültür", 4.7f, 150),
-        Restaurant(R.drawable.hamburgerr, "Burger King", "Güvercin, Kültür", 4.7f, 150),
-        Restaurant(R.drawable.hamburgerr, "Burger King", "Güvercin, Kültür", 4.7f, 150),
-        Restaurant(R.drawable.hamburgerr, "Burger King", "Güvercin, Kültür", 4.7f, 150),
-        Restaurant(R.drawable.hamburgerr, "Burger King", "Güvercin, Kültür", 4.7f, 150),
+        Restaurant(R.drawable.dukkan9, "Çorbacı Şükrü", "Kültür, Antalya", 4.1f, 500),
+        Restaurant(R.drawable.dukkan10, "Dürümcü Bedir", "Konyaaltı, Antalya", 3.6f, 400),
+        Restaurant(R.drawable.dukkan3, "Burger King", "Güvercin, Kültür", 4.7f, 150),
+        Restaurant(R.drawable.dukkan11, "Burger King", "Güvercin, Kültür", 4.7f, 150),
+        Restaurant(R.drawable.dukkan5, "Burger King", "Güvercin, Kültür", 4.7f, 150),
+        Restaurant(R.drawable.dukkan6, "Burger King", "Güvercin, Kültür", 4.7f, 150),
+        Restaurant(R.drawable.dukkan7, "Burger King", "Güvercin, Kültür", 4.7f, 150),
     )
+    TopRestaurantsSection(restaurants = restaurants)
+}
 
-    Column(
-                modifier = Modifier
-                    .fillMaxWidth()
-            ) {
-                TopRestaurantsSection(restaurants = restaurants)
-            }
-        }
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview4545() {
