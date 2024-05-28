25b180d5c864680c3a24f10fda13470d8eb9b0a7
Mustafa ozger <ozgermustafa118@gmail.com>
2024-05-28 17:46:06 +0300

    Restaurant and Food model updated

--- app/src/main/java/com/example/fooddeliveryproject/Models/Restaurant.kt
+++ app/src/main/java/com/example/fooddeliveryproject/Models/Restaurant.kt
@@ -3,7 +3,10 @@ package com.example.fooddeliveryproject.Models
 data class Restaurant(
     val id:Int,
     val name:String,
-    val image:String,
+    val imageUrl:String,
     val foodList: ArrayList<Food>,
-    val soldFood:ArrayList<Food>
-)
+    val soldFood:ArrayList<Food>,
+    var commentCount:Int=0,
+    var starRate:Double=0.0,
+
+    )
