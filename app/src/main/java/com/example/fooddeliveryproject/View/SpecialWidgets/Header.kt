4f91efa4c20525bdf1712538fed6d14f00bc4837
İsmail Babacan <114854598+ismailbabacann@users.noreply.github.com>
2024-06-11 19:54:37 +0300

    OrderStatus OrderHistory Pages added and Header has uptaded

--- app/src/main/java/com/example/fooddeliveryproject/View/SpecialWidgets/Header.kt
+++ app/src/main/java/com/example/fooddeliveryproject/View/SpecialWidgets/Header.kt
@@ -1,10 +1,12 @@
 package com.example.fooddeliveryproject.View.SpecialWidgets
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -16,6 +18,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontVariation.weight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.fooddeliveryproject.R
@@ -28,25 +31,32 @@ fun Header(){
     Row(
             verticalAlignment = Alignment.CenterVertically,
             horizontalArrangement = Arrangement.SpaceBetween,
-        //modifier = Modifier.fillMaxWidth()
         ) {
         BoxWithResource(resId = R.drawable.location, description = "menu")
         Column {
-            Row {
-                Text(text = "EV")
+            Row ( modifier = Modifier.fillMaxWidth()){
+                Column {
+                    Row {
+                        Text(text = "EV")
+                        Icon(
+                            painter = painterResource(id = R.drawable.arrow_down),
+                            contentDescription = "down",
+                            modifier = Modifier.size(16.dp),
+                            tint = Orange500
+
+                        )
+                    }
+                    Text(text = "Kepez,Antalya", color = Color.Gray)
+                }
                 Icon(
-                    painter = painterResource(id = R.drawable.arrow_down),
-                    contentDescription = "down",
-                    modifier = Modifier.size(16.dp),
-                    tint = Orange500
-
+                    painter = painterResource(id = R.drawable.baseline_shopping_cart_24),
+                    contentDescription ="aktif",
+                    modifier = Modifier.size(32.dp),
+                    tint = Orange500,
                 )
-
             }
-            Text(text = "Kepez,Antalya", modifier = Modifier.fillMaxWidth(), color = Color.Gray)
+
         }
-
-
 
 
         }
@@ -77,6 +87,9 @@ fun BoxWithResource(
             modifier = Modifier.size(iconSize!!.dp),
             tint = Orange500
             )
+        Row {
+
+        }
 
     }
 
