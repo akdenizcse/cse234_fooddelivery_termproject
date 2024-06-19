ca2e53b9c60aa999840bd84b757025d6b18d189b
Mustafa ozger <ozgermustafa118@gmail.com>
2024-06-20 00:24:36 +0300

    Restaurant view page update

--- app/src/main/java/com/example/fooddeliveryproject/View/Pages/RestorantsPage.kt
+++ app/src/main/java/com/example/fooddeliveryproject/View/Pages/RestorantsPage.kt
@@ -8,6 +8,8 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -20,29 +22,27 @@ import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
+import com.example.fooddeliveryproject.Models.Restaurant
 import com.example.fooddeliveryproject.R
+import com.example.fooddeliveryproject.Utils.downladImage
 import com.example.fooddeliveryproject.ViewModel.RestaurantViewModel
 
-data class Restaurant(
-    val imageRes: Int,
-    val name: String,
-    val location: String,
-    val rating: Double,
-    val reviews: Int
-)
 
 @Composable
 fun RestaurantPage(navigate: NavHostController = rememberNavController(),restaurantVM:RestaurantViewModel= viewModel()) {
-    val restaurants = listOf(
-        Restaurant(R.drawable.dukkan1, "Çorbacı Şükrü", "Kültür, Antalya", 4.1, 500),
-        Restaurant(R.drawable.dukkan6, "Hastayım Döner", "Konyaaltı, Antalya", 3.6, 400),
-        Restaurant(R.drawable.dukkan12, "Burger King", "Güvenen, Kültür", 4.7, 150),
-        Restaurant(R.drawable.dukkan11, "Makarna Yıldızı", "Çıralı, Antalya", 2.8, 200),
-        Restaurant(R.drawable.dukkan13, "Gogalo Döner", "Kültür, Antalya", 4.1, 500),
-        Restaurant(R.drawable.dukkan16, "Asker Abi'nin Yeri", "Konyaaltı, Antalya", 3.6, 400),
-        Restaurant(R.drawable.dukkan18, "McDonalds", "Güvenen, Kültür", 4.7, 150),
-        Restaurant(R.drawable.dukkan11, "Komagene", "Çıralı, Antalya", 2.8, 200)
-    )
+//    val restaurants = listOf(
+//        Restaurant(R.drawable.dukkan1, "Çorbacı Şükrü", "Kültür, Antalya", 4.1, 500),
+//        Restaurant(R.drawable.dukkan6, "Hastayım Döner", "Konyaaltı, Antalya", 3.6, 400),
+//        Restaurant(R.drawable.dukkan12, "Burger King", "Güvenen, Kültür", 4.7, 150),
+//        Restaurant(R.drawable.dukkan11, "Makarna Yıldızı", "Çıralı, Antalya", 2.8, 200),
+//        Restaurant(R.drawable.dukkan13, "Gogalo Döner", "Kültür, Antalya", 4.1, 500),
+//        Restaurant(R.drawable.dukkan16, "Asker Abi'nin Yeri", "Konyaaltı, Antalya", 3.6, 400),
+//        Restaurant(R.drawable.dukkan18, "McDonalds", "Güvenen, Kültür", 4.7, 150),
+//        Restaurant(R.drawable.dukkan11, "Komagene", "Çıralı, Antalya", 2.8, 200)
+//    )
+
+    restaurantVM.getRestaurantList()
+    val restaurantList by restaurantVM._restaurantList.observeAsState()
 
     Scaffold(
         topBar = {
@@ -72,17 +72,20 @@ fun RestaurantPage(navigate: NavHostController = rememberNavController(),restaur
             )
         },
         content = { paddingValues ->
-            LazyColumn(
-                contentPadding = paddingValues,
-                modifier = Modifier
-                    .fillMaxSize()
-                    .background(Color.White),
-                verticalArrangement = Arrangement.spacedBy(1.dp)
-            ) {
-                items(restaurants) { restaurant ->
-                    RestaurantItem(restaurant)
+            if (restaurantList!=null){
+                LazyColumn(
+                    contentPadding = paddingValues,
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(Color.White),
+                    verticalArrangement = Arrangement.spacedBy(1.dp)
+                ) {
+                    items(restaurantList!!) { restaurant ->
+                        RestaurantItem(restaurant)
+                    }
                 }
             }
+
         }
     )
 }
@@ -101,11 +104,7 @@ fun RestaurantItem(restaurant: Restaurant) {
                 .padding(8.dp),
             verticalAlignment = Alignment.CenterVertically
         ) {
-            Image(
-                painter = painterResource(id = restaurant.imageRes),
-                contentDescription = restaurant.name,
-                modifier = Modifier.size(64.dp)
-            )
+            downladImage(imageUrl = restaurant.imageUrl, size = 80)
             Spacer(modifier = Modifier.width(16.dp))
             Column(
                 modifier = Modifier.weight(1f)
@@ -116,11 +115,11 @@ fun RestaurantItem(restaurant: Restaurant) {
                     fontWeight = FontWeight.Bold,
                     color = Color.Black
                 )
-                Text(
-                    text = restaurant.location,
-                    fontSize = 14.sp,
-                    color = Color.Gray
-                )
+//                Text(
+//                    text = restaurant.location,
+//                    fontSize = 14.sp,
+//                    color = Color.Gray
+//                )
             }
             Spacer(modifier = Modifier.width(16.dp))
             Column(
@@ -137,14 +136,14 @@ fun RestaurantItem(restaurant: Restaurant) {
                     )
                     Spacer(modifier = Modifier.width(4.dp))
                     Text(
-                        text = "${restaurant.rating}",
+                        text = "${restaurant.starRate}",
                         fontSize = 14.sp,
                         fontWeight = FontWeight.Bold,
                         color = Color.Black
                     )
                 }
                 Text(
-                    text = "(${restaurant.reviews}+ yorum)",
+                    text = "(${restaurant.commentCount}+ yorum)",
                     fontSize = 12.sp,
                     color = Color.Gray
                 )
